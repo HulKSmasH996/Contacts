06290c311abb86b9e796a7eb5b45ef0e88841591
Subhojeet Sen <1421036@gmail.com>
2017-05-10 21:17:02 +0530

    Retriving Name , Phone no. , Email Id

---
+++ app/src/main/java/com/example/viper/contacts/MainActivity.java
@@ -0,0 +1,250 @@
+package com.example.viper.contacts;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends Activity {
+    private static final int PERMISSION_REQUEST_CONTACT = 1;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //readContacts();
+        askForContactPermission();
+    }
+
+    public void readContacts(){
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+
+
+//                if (cur.getInt(cur.getColumnIndex(
+//                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+//                    Cursor pCur = cr.query(
+//                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+//                            null,
+//                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+//                            new String[]{id}, null);
+//                    while (pCur.moveToNext()) {
+//                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+//                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+//                        Toast.makeText(getApplicationContext(), "Name: " + name
+//                                + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
+//                    }
+//                    pCur.close();
+//                }
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                   // System.out.println("name : " + name + ", ID : " + id);
+
+                    // get the phone number
+                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phone = pCur.getString(
+                                pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//                        Toast.makeText(getApplicationContext(), "Name: " + name
+//                               + ", Phone No: " + phone, Toast.LENGTH_SHORT).show();
+                       // System.out.println("phone" + phone);
+                    }
+                    pCur.close();
+
+
+                    // get email and type
+
+                    Cursor emailCur = cr.query(
+                            ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (emailCur.moveToNext()) {
+                        // This would allow you get several email addresses
+                        // if the email addresses were stored in an array
+                        String email = emailCur.getString(
+                                emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                        String emailType = emailCur.getString(
+                                emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE));
+                        Toast.makeText(getApplicationContext(), "Name: " + name
+                               + ", Email: " + email, Toast.LENGTH_LONG).show();
+
+                        System.out.println("Email " + email + " Email Type : " + emailType);
+
+                    }
+                    emailCur.close();
+
+                    // Get note.......
+//                    String noteWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+//                    String[] noteWhereParams = new String[]{id,
+//                            ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE};
+//                    Cursor noteCur = cr.query(ContactsContract.Data.CONTENT_URI, null, noteWhere, noteWhereParams, null);
+//                    if (noteCur.moveToFirst()) {
+//                        String note = noteCur.getString(noteCur.getColumnIndex(ContactsContract.CommonDataKinds.Note.NOTE));
+//                        System.out.println("Note " + note);
+//                    }
+//                    noteCur.close();
+
+                    //Get Postal Address....
+
+//                    String addrWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+//                    String[] addrWhereParams = new String[]{id,
+//                            ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE};
+//                    Cursor addrCur = cr.query(ContactsContract.Data.CONTENT_URI,
+//                            null, null, null, null);
+//                    while(addrCur.moveToNext()) {
+//                        String poBox = addrCur.getString(
+//                                addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POBOX));
+//                        String street = addrCur.getString(
+//                                addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
+//                        String city = addrCur.getString(
+//                                addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY));
+//                        String state = addrCur.getString(
+//                                addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.REGION));
+//                        String postalCode = addrCur.getString(
+//                                addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE));
+//                        String country = addrCur.getString(
+//                                addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY));
+//                        String type = addrCur.getString(
+//                                addrCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.TYPE));
+//
+//                        // Do something with these....
+//
+//                    }
+//                    addrCur.close();
+
+                    // Get Instant Messenger.........
+//                    String imWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+//                    String[] imWhereParams = new String[]{id,
+//                            ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE};
+//                    Cursor imCur = cr.query(ContactsContract.Data.CONTENT_URI,
+//                            null, imWhere, imWhereParams, null);
+//                    if (imCur.moveToFirst()) {
+//                        String imName = imCur.getString(
+//                                imCur.getColumnIndex(ContactsContract.CommonDataKinds.Im.DATA));
+//                        String imType;
+//                        imType = imCur.getString(
+//                                imCur.getColumnIndex(ContactsContract.CommonDataKinds.Im.TYPE));
+//                    }
+//                    imCur.close();
+
+                    // Get Organizations.........
+
+//                    String orgWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+//                    String[] orgWhereParams = new String[]{id,
+//                            ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE};
+//                    Cursor orgCur = cr.query(ContactsContract.Data.CONTENT_URI,
+//                            null, orgWhere, orgWhereParams, null);
+//                    if (orgCur.moveToFirst()) {
+//                        String orgName = orgCur.getString(orgCur.getColumnIndex(ContactsContract.CommonDataKinds.Organization.DATA));
+//                        String title = orgCur.getString(orgCur.getColumnIndex(ContactsContract.CommonDataKinds.Organization.TITLE));
+//                    }
+//                    orgCur.close();
+
+                }
+            }
+        }}
+    public void askForContactPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                    builder.setTitle("Contacts access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("please confirm Contacts access");//TODO put real question
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.READ_CONTACTS}
+                                    , PERMISSION_REQUEST_CONTACT);
+                        }
+                    });
+                    builder.show();
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            PERMISSION_REQUEST_CONTACT);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }else{
+                readContacts();
+            }
+        }
+        else{
+            readContacts();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CONTACT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readContacts();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied",Toast.LENGTH_LONG).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+    }
+
